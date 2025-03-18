ab8e9bc3b92cff3ef55c8e867092d1fe262217fb
mwsundberg <gitcommits@mws.is>
2025-03-18 05:18:35 -0700

    Cleanup util.js

--- global/utils.js
+++ global/utils.js
@@ -1,49 +1,8 @@
 /**
- * Get a well-formed host to match against from a URL user- or code-supplied
- * @param {string} text A URL-like value (eg `ftp://example.com/file/path/etc`, `google.com/`)
- * @returns {string} Well formatted host portion of url (eg `example.com`, `google.com`)
- * 
- * @throws Parsing an invalid URL
- */
-export function extractURLHost(text) {
-    let url = text + ""; // cast to string (is this needed?)
-
-    // We don't actually care about the protocol as we only compare url.host
-    // But the URL object will fail to create if no protocol is provided
-    if (!url.startsWith("http")) {
-        url = "https://" + url;
-    }
-    const newUrl = new URL(url);
-    return newUrl.host;
-}
-
-/**
  * Object-specific empty check
  * @param {Object} object
  * @returns {boolean} 
  */
 export function isObjectEmpty(object) {
     return object === undefined || object === null || Object.keys(object).length === 0;
-}
-
-/**
- * Check if a value is "empty".
- * 
- * `0` and `false` *aren't* considered empty.
- * 
- * Nullish values, empty strings, and empty `Set`s are considered empty.
- * @param {any} value 
- * @returns {boolean} 
- */
-export function isEmptyValue(value) {
-    // Sets seem empty when treated as objects
-    if(value instanceof Set && value.size !== 0) return false;
-
-    // Main check
-    return (
-        (value === undefined) ||
-        (value === null) ||
-        (value === "") ||
-        (typeof value === "object" && isObjectEmpty(value)) // Also catches empty arrays and Sets
-    );
 }
--- settings/settings.js
+++ settings/settings.js
@@ -1,6 +1,5 @@
 import { getItemFromLocal, modifyItemInLocal } from "../global/BrowserStorageManager.js";
 import { createElement } from "../global/domUtils.js";
-import { extractURLHost } from "../global/utils.js";
 
 /**
  * A single row in the allowed domain list display
@@ -85,6 +84,25 @@ async function load_allowed_domains() {
 
     // Toggle visibility on the container wrapper at end
     table.removeAttribute("hidden");
+}
+
+/**
+ * Get a well-formed host to match against from a URL user- or code-supplied
+ * @param {string} text A URL-like value (eg `ftp://example.com/file/path/etc`, `google.com/`)
+ * @returns {string} Well formatted host portion of url (eg `example.com`, `google.com`)
+ * 
+ * @throws Parsing an invalid URL
+ */
+export function extractURLHost(text) {
+    let url = text + ""; // cast to string (is this needed?)
+
+    // We don't actually care about the protocol as we only compare url.host
+    // But the URL object will fail to create if no protocol is provided
+    if (!url.startsWith("http")) {
+        url = "https://" + url;
+    }
+    const newUrl = new URL(url);
+    return newUrl.host;
 }
 
 async function saveOptions(e) {
