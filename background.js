8195b037e26f7f1b1d479edb125e5984ab66ec77
mwsundberg <gitcommits@mws.is>
2025-03-19 17:44:32 -0700

    Normalize Fully Qualified Domain Names and hoisted all URL parsing (need to revert, worse performance)

--- background.js
+++ background.js
@@ -15,15 +15,70 @@ async function startup(){
 	}
 }
 
+/**
+ * Normalize a {@link https://en.wikipedia.org/wiki/Fully_qualified_domain_name | Fully Qualified Domain Name}
+ * (aka remove a trailing dot from the hostname).
+ * 
+ * @param {URL} [url] A URL object to normalize
+ * @returns {void} No return, mutates the passed object
+ */
+function normalizeFQDN(url) {
+    if(!url) return;
+
+    // Nullish check at end is to accomodate not finding a regex match, which is technically impossible, but anyways.
+    url.hostname = url.hostname.match(/^(.*?)\.?$/)?.[1] ?? "";
+}
+
+
+/**
+ * Example request URL structure for discussing different URL interpretations: *"protocol://user:pass@sub.example.tld.:1234/path/?query#hash"*
+ * Restrictive access checks should be broad and check against the `hostname` URL property (ie. "sub.example.tld.", ignoring port and protocol)
+ * Permissive access checks should cautious and check against the `host` URL property (ie. "sub.example.tld.:1234", ignoring protocol)
+ * @param {{originUrl: string, documentUrl: string, thirdParty: boolean, url: string, tabId: number}} requestDetails 
+ * @returns {Promise<{ cancel: boolean }>} Whether to block the request or not
+ */
 async function cancel(requestDetails) {
-    // First check the whitelist
-    let check_allowed_url;
+    // Extract values out
+    const {originUrl: _originUrl, documentUrl: _documentUrl, thirdParty, url: _request, tabId} = requestDetails;
+    if(_originUrl !== _documentUrl || !_originUrl || tabId === -1) console.warn("Abnormal request in `cancel()`:", requestDetails);
+    
+    // TODO delete this one fully understand request data passed
+    console.debug("Request:", requestDetails);
+
+    // URL parsing is almost globally needed
+    let originUrl, documentUrl, request;
     try {
-        check_allowed_url = new URL(requestDetails.originUrl);
-    } catch {
-        console.error("Aborted filtering on domain due to unparseable domain: ", requestDetails.originUrl);
-        return { cancel: false }; // invalid origin
+        // Only try parsing the URL if it's there
+        // TODO relocate unneeded parsings to after all early returns (this was undone by the hoisting here, yet wanted it just for convenience's sake)
+        // TODO determine specific criteria for when URLs aren't present
+        // - Suspect WebWorker and ServiceWorker requests won't have `originUrl`
+        // - Suspect direct page loads won't have `documentUrl`
+        if(_originUrl) originUrl = new URL(_originUrl);
+        if(_documentUrl) documentUrl = new URL(_documentUrl);
+        if(_request) request =  new URL(_request);
+    } catch(error) {
+        console.error("Aborted filtering due to unparseable domain(s): ", {originUrl, request, documentUrl});
+        return { cancel: false };
     }
+
+    // Catch no originUrl or request provided
+    if(!originUrl || !request) {
+        console.error("Aborted filtering due to no `originUrl` or request `url`: ", requestDetails);
+        return { cancel: false };
+    }
+
+    // Makes `localhost.:8080` be `localhost:8080` to make tricking the filtering harder
+    normalizeFQDN(originUrl);
+    normalizeFQDN(documentUrl);
+    normalizeFQDN(request);
+
+    // Request probably comes from a subresource like an iframe. Could decide to handle differently
+    // TODO handle separately
+    if(documentUrl && documentUrl.host !== originUrl.host) {
+        console.warn("Potential subresource load attempted:", {document: documentUrl.host, origin: originUrl.host, request: request});
+    }
+    // First check the allowlist
+    let check_allowed_url = originUrl;
 
     const allowed_domains_list = await getItemFromLocal("allowed_domain_list", []);
     // Perform an exact match against the whitelisted domains (dont assume subdomains are allowed)
@@ -44,12 +99,12 @@ async function cancel(requestDetails) {
     let is_requested_local = local_filter.test(requestDetails.url);
     // Make sure we are not searching the CNAME of local addresses
     if (!is_requested_local) {
-        let url = new URL(requestDetails.url);
+        let url = request;
         // Send a request to get the CNAME of the webrequest
         let resolving = await browser.dns.resolve(url.host, ["canonical_name"]);
         // If the CNAME redirects to a online-metrix.net domain -> Block
         if (thm.test(resolving.canonicalName)) {
-            console.debug("Blocking domain for being a threatmetrix match: ", {url: url, cname: resolving.canonicalName});
+            console.debug("Blocking domain for being a threatmetrix match: ", {url, cname: resolving.canonicalName});
             increaseBadge(requestDetails, true); // increment badge and alert
             addBlockedTrackingHost(url, requestDetails.tabId);
             return { cancel: true };
@@ -60,11 +115,13 @@ async function cancel(requestDetails) {
     if (is_requested_local) {
         // If URL in the address bar is a local address dont block the request
         if (!local_filter.test(requestDetails.originUrl)) {
-            let url = new URL(requestDetails.url);
+            let url = request;
             console.debug("Blocking domain for portscanning: ", url);
             increaseBadge(requestDetails, false); // increment badge and alert
             addBlockedPortToHost(url, requestDetails.tabId);
             return { cancel: true };
+        } else {
+            if(originUrl !== request) console.warn("Not blocking local-to-local access:", {originUrl, request});
         }
     }
     // Dont block sites that don't alert the detection
